78aad7e37586ef4b5a922abb27cb60fc5d84e465
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-11-10 23:56:15 +0100

    WIP video stream

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,46 @@
 
 // Html elements
+const { desktopCapturer, remote } = require('electron')
+const { Menu } = remote
 const videoElement = document.getElementById('video')
 const startBtn = document.getElementById('startBtn')
 const stopBtn = document.getElementById('stopBtn')
 const videoSelectBtn = document.getElementById('videoSelectBtn')
-const { getVideoResources } = require('./utils/getVideoResources')
+
+const selectSource = async (source) => {
+  videoSelectBtn.innterText = source.name
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  }
+
+  const stream = await navigator.mediaDevices.getUserMedia(constraints)
+
+  videoElement.srcObject = stream
+  videoElement.play
+}
+
+const getVideoResources = async () => {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  })
+  
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      }
+    })
+    )
+
+    videoOptionsMenu.popup()
+  }
 
 videoSelectBtn.onclick = getVideoResources
